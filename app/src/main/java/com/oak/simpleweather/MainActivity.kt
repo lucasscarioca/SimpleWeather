7471597a36dd6c64c41ed98763ebb404aa03fc0f
Lucas Carioca <lucascarioca97@gmail.com>
2022-08-15 17:34:22 -0300

    Setting up packages/files structure and WeatherApplication class

--- app/src/main/java/com/oak/simpleweather/MainActivity.kt
+++ app/src/main/java/com/oak/simpleweather/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.oak.simpleweather.ui.theme.SimpleWeatherTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/oak/simpleweather/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.oak.simpleweather.di
+
+class AppModule {
+}
