85eb85eed1ae23a8745917c66752256bbd23c9c6
Lucas Carioca <lucascarioca97@gmail.com>
2022-08-17 22:45:05 -0300

    Created WeatherAppBar Composable

--- app/src/main/java/com/oak/simpleweather/screens/main/MainScreen.kt
+++ app/src/main/java/com/oak/simpleweather/screens/main/MainScreen.kt
@@ -1,21 +1,22 @@
 package com.oak.simpleweather.screens.main
 
+import android.annotation.SuppressLint
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.oak.simpleweather.data.DataOrException
 import com.oak.simpleweather.model.Weather
+import com.oak.simpleweather.widgets.WeatherAppBar
 
 @Composable
 fun MainScreen(navController: NavController, viewModel: MainViewModel = hiltViewModel()) {
-    ShowData(viewModel)
-}
-
-@Composable
-fun ShowData(viewModel: MainViewModel) {
 
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true)) {
@@ -25,7 +26,31 @@ fun ShowData(viewModel: MainViewModel) {
     if (weatherData.loading == true){
         CircularProgressIndicator()
     } else if (weatherData.data != null) {
-        Text(text = "MainScreen ${weatherData.data!!}")
+        MainScaffold(weather = weatherData.data!!, navController)
     }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScaffold(weather: Weather, navController: NavController) {
+    
+    Scaffold(topBar = {
+        WeatherAppBar(
+            title = weather.city.name + ", ${weather.city.country}",
+            navController = navController,
+            elevation = 5.dp
+        ) {
+
+        }
+    }) {
+        MainContent(data = weather)
+    }
+
+}
+
+@Composable
+fun MainContent(data: Weather) {
+
+    Text(text = data.city.name)
 
 }
