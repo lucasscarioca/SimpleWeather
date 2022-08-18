c11c577c33238701b95a83941fbb32e4dd8d7502
Lucas Carioca <lucascarioca97@gmail.com>
2022-08-17 23:17:50 -0300

    working on MainContent Composable

--- app/src/main/java/com/oak/simpleweather/screens/main/MainScreen.kt
+++ app/src/main/java/com/oak/simpleweather/screens/main/MainScreen.kt
@@ -1,18 +1,28 @@
 package com.oak.simpleweather.screens.main
 
 import android.annotation.SuppressLint
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.oak.simpleweather.data.DataOrException
 import com.oak.simpleweather.model.Weather
+import com.oak.simpleweather.utils.formatDate
+import com.oak.simpleweather.utils.formatDecimals
 import com.oak.simpleweather.widgets.WeatherAppBar
 
 @Composable
@@ -20,7 +30,7 @@ fun MainScreen(navController: NavController, viewModel: MainViewModel = hiltView
 
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true)) {
-        value = viewModel.getWeatherData(city = "Seattle")
+        value = viewModel.getWeatherData(city = "ohio")
     }.value
 
     if (weatherData.loading == true){
@@ -51,6 +61,76 @@ fun MainScaffold(weather: Weather, navController: NavController) {
 @Composable
 fun MainContent(data: Weather) {
 
-    Text(text = data.city.name)
+    val weatherItem = data.list[0]
+    val imageUrl = "https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png"
+
+    Column(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = formatDate(weatherItem.dt),
+            style = MaterialTheme.typography.caption,
+            color = MaterialTheme.colors.onSecondary,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp)
+        )
+
+        Surface(
+            modifier = Modifier
+                .padding(4.dp)
+                .size(200.dp),
+            shape = CircleShape,
+            color = Color(0xFFFFC400)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                WeatherStateImage(imageUrl = imageUrl)
+
+                //Temperature
+                Text(
+                    text = formatDecimals(weatherItem.temp.day) + "º",
+                    style = MaterialTheme.typography.h4,
+                    fontWeight = FontWeight.ExtraBold
+                )
+
+                //Weather
+                Text(
+                    text = weatherItem.weather[0].main,
+                    fontStyle = FontStyle.Italic
+                )
+
+            }
+
+        }
+
+    }
 
 }
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    Image(
+        painter = rememberAsyncImagePainter(model = imageUrl),
+        contentDescription = "icon image",
+        modifier = Modifier.size(80.dp)
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
