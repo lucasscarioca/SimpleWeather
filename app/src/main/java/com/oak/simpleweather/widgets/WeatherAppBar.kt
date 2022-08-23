21ea92933cb7c3971eb39cd9111ae6f48c4c926c
Lucas Carioca <lucascarioca97@gmail.com>
2022-08-22 22:22:07 -0300

    Finished FavoritesScreen and add/remove from favorites funcionalities

--- app/src/main/java/com/oak/simpleweather/widgets/WeatherAppBar.kt
+++ app/src/main/java/com/oak/simpleweather/widgets/WeatherAppBar.kt
@@ -1,27 +1,31 @@
 package com.oak.simpleweather.widgets
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.oak.simpleweather.model.Favorite
 import com.oak.simpleweather.navigation.WeatherScreens
+import com.oak.simpleweather.screens.favorites.FavoriteViewModel
 
 @Composable
 fun WeatherAppBar(
@@ -30,6 +34,7 @@ fun WeatherAppBar(
     isMainScreen: Boolean = true,
     elevation: Dp = 0.dp,
     navController: NavController,
+    favoriteViewModel: FavoriteViewModel = hiltViewModel(),
     onAddActionClicked: () -> Unit = {},
     onButtonClicked: () -> Unit = {}
 ) {
@@ -37,6 +42,16 @@ fun WeatherAppBar(
     val showDialog = remember {
         mutableStateOf(false)
     }
+
+    val showIt = remember {
+        mutableStateOf(false)
+    }
+
+    val toastText = remember {
+        mutableStateOf("Favorites updated")
+    }
+
+    val context = LocalContext.current
 
     if (showDialog.value) {
         ShowSettingDropDownMenu(showDialog = showDialog, navController = navController)
@@ -69,21 +84,83 @@ fun WeatherAppBar(
                   } else Box {}
         },
         navigationIcon = {
-                         if (icon != null) {
-                             Icon(
-                                 imageVector = icon,
-                                 contentDescription = null,
-                                 tint = MaterialTheme.colors.onSecondary,
-                                 modifier = Modifier.clickable {
-                                     onButtonClicked.invoke()
-                                 }
-                             )
+                 if (icon != null) {
+                     Icon(
+                         imageVector = icon,
+                         contentDescription = null,
+                         tint = MaterialTheme.colors.onSecondary,
+                         modifier = Modifier.clickable {
+                             onButtonClicked.invoke()
                          }
+                     )
+                 }
+                if (isMainScreen) {
+                    val isAlreadyFavList = favoriteViewModel
+                        .favList.collectAsState().value.filter { item ->
+                            (item.city == title.split(",")[0])
+                        }
+                    // City is not favorite
+                    if (isAlreadyFavList.isEmpty()) {
+                        Icon(
+                            imageVector = Icons.Default.FavoriteBorder,
+                            contentDescription = "Favorite icon",
+                            modifier = Modifier
+                                .scale(0.9f)
+                                .padding(start = 10.dp)
+                                .clickable {
+                                    val locationList = title.split(",")
+                                    favoriteViewModel.insertFavorite(
+                                        Favorite(
+                                            city = locationList[0],
+                                            country = locationList[1]
+                                        )
+                                    ).run {
+                                        showIt.value = true
+                                        toastText.value = "Added to favorites"
+                                    }
+                                }
+                        )
+                    // City is favorite
+                    } else {
+                        Icon(
+                            imageVector = Icons.Default.Favorite,
+                            contentDescription = "Favorite icon",
+                            modifier = Modifier
+                                .scale(0.9f)
+                                .padding(start = 10.dp)
+                                .clickable {
+                                    val favCity = favoriteViewModel
+                                        .favList.value.find {
+                                        (it.city == title.split(",")[0])
+                                    }
+                                    if (favCity != null) {
+                                        favoriteViewModel.deleteFavorite(favCity).run {
+                                            showIt.value = true
+                                            toastText.value = "Removed from favorites"
+                                        }
+                                    }
+                                },
+                            tint = Color.Red.copy(alpha = 0.6f)
+                        )
+                    }
+
+                    ShowToast(context = context, showIt, toastText)
+
+
+                }
         },
         backgroundColor = Color.Transparent,
         elevation = elevation
     )
     
+}
+
+@Composable
+fun ShowToast(context: Context, showIt: MutableState<Boolean>, toastText: MutableState<String>) {
+    if (showIt.value) {
+        Toast.makeText(context,toastText.value, Toast.LENGTH_SHORT).show()
+        showIt.value = false
+    }
 }
 
 @Composable
@@ -100,7 +177,10 @@ fun ShowSettingDropDownMenu(showDialog: MutableState<Boolean>, navController: Na
     ) {
         DropdownMenu(
             expanded = expanded,
-            onDismissRequest = { expanded = false },
+            onDismissRequest = {
+                expanded = false
+                showDialog.value = false
+            },
             modifier = Modifier
                 .width(140.dp)
                 .background(Color.White)
