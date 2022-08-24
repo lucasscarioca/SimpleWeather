e4cce764729c4074c05d12634735c31e60332d46
Lucas Carioca <lucascarioca97@gmail.com>
2022-08-23 21:06:18 -0300

    working on SettingsScreen and favorite measurement persistence

--- app/src/main/java/com/oak/simpleweather/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/oak/simpleweather/screens/settings/SettingsScreen.kt
@@ -1,18 +1,96 @@
 package com.oak.simpleweather.screens.settings
 
 import android.annotation.SuppressLint
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.oak.simpleweather.model.Unit
+import com.oak.simpleweather.widgets.WeatherAppBar
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun SettingsScreen(navController: NavController, settingsViewModel: SettingsViewModel = hiltViewModel()) {
 
-    Scaffold(topBar = {}) {
+    var unitToggleState by remember { mutableStateOf(false) }
+    val measurementUnits = listOf("Imperial (F)", "Metric (C)")
+    var choiceState by remember { mutableStateOf("") }
 
+    Scaffold(topBar = {
+        WeatherAppBar(
+            title = "Settings",
+            icon = Icons.Default.ArrowBack,
+            isMainScreen = false,
+            navController = navController
+        )
+    }) {
+        Surface(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = CenterHorizontally
+            ) {
+                Text(
+                    text = "Change Units of Measurement",
+                    modifier = Modifier.padding(bottom = 15.dp)
+                )
+                
+                IconToggleButton(
+                    checked = !unitToggleState,
+                    onCheckedChange = {
+                        unitToggleState = !it
+                        if (unitToggleState) {
+                            choiceState = "Imperial (F)"
+                        } else {
+                            choiceState = "Metric (C)"
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .clip(shape = RectangleShape)
+                        .padding(5.dp)
+                        .background(Color.Magenta.copy(alpha = 0.4f))
+                ) {
+                    
+                    Text(text = if (unitToggleState) "Fahrenheit ºF" else "Celsius ºC")
+
+                }
+                
+                Button(
+                    onClick = {
+                        settingsViewModel.deleteAllUnits()
+                        settingsViewModel.insertUnit(Unit(
+                            unit = choiceState
+                        ))
+                    },
+                    modifier = Modifier
+                        .padding(3.dp)
+                        .align(CenterHorizontally),
+                    shape = RoundedCornerShape(34.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFEFBE42))
+                ) {
+                    Text(
+                        text = "Save",
+                        modifier = Modifier.padding(4.dp),
+                        color = Color.White,
+                        fontSize = 17.sp
+                    )
+                }
+            }
+        }
     }
-
 }
